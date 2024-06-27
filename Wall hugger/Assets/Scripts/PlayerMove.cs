b668791e307015826bec663d25ca3e930c15e971
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2024-06-27 13:31:22 +1000

    Movemnet & jumping works

--- Wall hugger/Assets/Scripts/PlayerMove.cs
+++ Wall hugger/Assets/Scripts/PlayerMove.cs
@@ -5,8 +5,11 @@ using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private float gravity = 10;
-    [SerializeField] private float speed = 5;
+    [SerializeField] private float gravity = 20; // m/s/s
+    [SerializeField] private float speed = 5; // m/s
+    [SerializeField] private float jumpBufferTime = 0.1f; // seconds
+    [SerializeField] private float coyoteTime = 0.1f; // seconds
+    [SerializeField] private float jumpSpeed = 1f; // m/s
 
     private Actions actions;
     private InputAction moveAction;
@@ -15,9 +18,14 @@ public class PlayerMove : MonoBehaviour
     new private Rigidbody2D rigidbody;
     private Vector2 gravityDir;
     private Vector2 inputDir;
+    private Vector2 movementDir;
     private ContactPoint2D[] tempContacts = new ContactPoint2D[4];
     private List<ContactPoint2D> contacts = new List<ContactPoint2D>();
-    
+    private ContactPoint2D? activeContact = null;
+
+    private float lastJumpTime = float.NegativeInfinity;
+    private float lastContactTime = float.NegativeInfinity;
+
     void Awake()
     {
         actions = new Actions();
@@ -40,30 +48,100 @@ public class PlayerMove : MonoBehaviour
 
     void FixedUpdate()
     {
+        // movement
+        float movementScale = inputDir.magnitude;
+        movementDir = inputDir.normalized;
+        ResolveContacts();
+        Vector2 vMove = speed * movementScale * movementDir;
+
+        // do not change the component of movement in the gravity direction
+        Vector2 oldV = rigidbody.velocity;
+        float vDotG = Vector2.Dot(oldV, gravityDir);
+        Vector2 vDown = vDotG * gravityDir;
+
+        // jump
+        if (Time.time - lastContactTime < coyoteTime &&
+            Time.time - lastJumpTime < jumpBufferTime)
+        {
+            // jump 'upwards'
+            vDown = -jumpSpeed * gravityDir;
+            lastJumpTime = float.NegativeInfinity;
+        }
+        rigidbody.velocity = vDown + vMove;
+
         rigidbody.AddForce(gravity * gravityDir);
+
+        // clear contacts
         contacts.Clear();
+    }
+
+    private void ResolveContacts()
+    {
+        // remove downward components of the movement vector
+        for (int i = 0; i < contacts.Count; i++)
+        {
+            Vector2 normal = contacts[i].normal;
+            float vDotN = Vector2.Dot(movementDir, normal);
+
+            if (vDotN < 0)
+            {
+                // movement is pointing 'downwards'
+                // remove downward component
+                movementDir -= vDotN * normal;
+            }
+        }        
+
+        // find contact point with closest tangent plane
+        float minDot = 1f;
+        activeContact = null;
+
+        for (int i = 0; i < contacts.Count; i++)
+        {
+            Vector2 normal = contacts[i].normal;
+            float vDotN = Vector2.Dot(movementDir, normal);
+
+            if (vDotN < minDot)
+            {
+                activeContact = contacts[i];
+                minDot = vDotN;
+            }
+        }
+
+        // set gravity to most recent active contact's 'down' direction
+        if (activeContact != null)
+        {
+            lastContactTime = Time.time;
+            gravityDir = -activeContact.Value.normal;
+        }
+
+        // movement is always perpendicular to gravity
+        float vDotG = Vector2.Dot(movementDir, gravityDir);
+        movementDir -= vDotG * gravityDir;
     }
 
     void Update()
     {
         inputDir = moveAction.ReadValue<Vector2>();
+        bool jumpPressed = jumpAction.WasPerformedThisFrame();
+
+        if (jumpPressed) {
+            lastJumpTime = Time.time;
+        }
     }
 
+#region Collisions
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log($"Enter collider = {collision.collider.name}");
         RecordContacts(collision);
     }
 
     void OnCollisionStay2D(Collision2D collision)
     {
-        Debug.Log($"Stay collider = {collision.collider.name}");
         RecordContacts(collision);
     }
 
     void OnCollisionExit2D(Collision2D collision)
     {
-        Debug.Log($"Exit collider = {collision.collider.name}");
         RecordContacts(collision);
     }
 
@@ -86,20 +164,35 @@ public class PlayerMove : MonoBehaviour
             contacts.Add(tempContacts[i]);
         }        
     }
+#endregion Collisions
 
-
+#region Gizmos
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawLine(transform.position, transform.position + (Vector3)inputDir);
 
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine(transform.position, transform.position + (Vector3)movementDir);
+
         Gizmos.color = Color.blue;
         Gizmos.DrawLine(transform.position, transform.position + (Vector3)gravityDir);
 
-        Gizmos.color = Color.black;
         for (int i = 0; i < contacts.Count; i++)
         {
-            Gizmos.DrawSphere(contacts[i].point, 0.1f);
+            ContactPoint2D c = contacts[i];
+            Gizmos.color = Color.black;
+            Gizmos.DrawSphere(c.point, 0.1f);
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(c.point, c.point + c.normal);
+        }
+
+        if (activeContact.HasValue)
+        {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawSphere(activeContact.Value.point, 0.1f);
         }
     }
+#endregion Gizmos
+
 }
