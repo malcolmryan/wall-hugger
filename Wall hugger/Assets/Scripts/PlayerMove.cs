6edf67aa948a1ba5a2f7f73f43fb7dc59f4825ac
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2024-06-27 12:44:58 +1000

    Collecting collision points

--- Wall hugger/Assets/Scripts/PlayerMove.cs
+++ Wall hugger/Assets/Scripts/PlayerMove.cs
@@ -14,7 +14,10 @@ public class PlayerMove : MonoBehaviour
 
     new private Rigidbody2D rigidbody;
     private Vector2 gravityDir;
-
+    private Vector2 inputDir;
+    private ContactPoint2D[] tempContacts = new ContactPoint2D[4];
+    private List<ContactPoint2D> contacts = new List<ContactPoint2D>();
+    
     void Awake()
     {
         actions = new Actions();
@@ -38,10 +41,65 @@ public class PlayerMove : MonoBehaviour
     void FixedUpdate()
     {
         rigidbody.AddForce(gravity * gravityDir);
+        contacts.Clear();
     }
 
     void Update()
     {
-        
+        inputDir = moveAction.ReadValue<Vector2>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log($"Enter collider = {collision.collider.name}");
+        RecordContacts(collision);
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log($"Stay collider = {collision.collider.name}");
+        RecordContacts(collision);
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log($"Exit collider = {collision.collider.name}");
+        RecordContacts(collision);
+    }
+
+    private void RecordContacts(Collision2D collision)
+    {
+        // Note: this is called once per collider, on every physics frame
+        // So we need to collect the contacts into a list
+
+        // Resize the array if necessary
+        int nContacts = collision.contactCount;
+        if (nContacts > tempContacts.Length)
+        {
+            tempContacts = new ContactPoint2D[nContacts];
+        }
+
+        // Get contacts and add them to the list
+        collision.GetContacts(tempContacts);
+        for (int i = 0; i < nContacts; i++)
+        {
+            contacts.Add(tempContacts[i]);
+        }        
+    }
+
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + (Vector3)inputDir);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + (Vector3)gravityDir);
+
+        Gizmos.color = Color.black;
+        for (int i = 0; i < contacts.Count; i++)
+        {
+            Gizmos.DrawSphere(contacts[i].point, 0.1f);
+        }
     }
 }
